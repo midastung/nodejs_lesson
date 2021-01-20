97ec08ae2e04865b79efbe6a584b3d84263152c6
midastung <midas112800@gmail.com>
2021-01-20 14:41:25 +0800

    註解

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const dlsIndex = require('./routers/dlsIndex');
+const express = require('express');     //呼叫套件
+const app = express();      
+const dlsIndex = require('./routers/dlsIndex');     //設定router位址  
 
-app.listen(3000, ()=>{
+app.listen(3000, ()=>{                              //聽port位址
     console.log("Listen on port:" + '3000');
 });
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use('/', dlsIndex);
+app.set('views', './views');        //設定view位置
+app.set('view engine', 'ejs');      //範本檔設定
+app.use('/', dlsIndex);             //網址位址
